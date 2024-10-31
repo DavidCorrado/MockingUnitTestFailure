1247ca5568a7a30f00e90d4ecf45f9d109b3bf1c
David Corrado <david.corrado@well.co>
2024-10-31 10:36:23 -0500

    Bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,11 @@ plugins {
 
 android {
     namespace = "com.example.mocktest"
-    compileSdk = 34
-
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.example.mocktest"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -56,4 +55,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    testImplementation(libs.mockk)
+    testImplementation(libs.robolectric)
 }
--- app/src/test/java/com/example/mocktest/ExampleUnitTest.kt
+++ app/src/test/java/com/example/mocktest/ExampleUnitTest.kt
@@ -1,17 +1,33 @@
 package com.example.mocktest
 
+import android.app.Application
+import io.mockk.clearAllMocks
+import io.mockk.mockk
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(RobolectricTestRunner::class)
+@Config(instrumentedPackages = ["androidx.loader.content"], application = Application::class)
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `test 1`() {
+        val onFeedBackCardClicked = mockk<() -> Unit>(relaxed = true)
+    }
+
+    @Test
+    @GraphicsMode(GraphicsMode.Mode.NATIVE)
+    fun `test 2`() {
+        val onSubmitFeedback: () -> Unit = mockk(relaxed = true)
     }
 }
